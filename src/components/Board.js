a2cfe9361422b88fbb0eab8cdaee3303fe08b5fb
sklee <otherluk92@gmail.com>
2023-05-15 19:06:20 +0900

    'calculatoeWinner_func'

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Square from './Square'
 import "./Board.css";
 
-export default class Board extends Component {
 
-renderSquare(i) {
-  return <Square value={i} />
-}
+const Board = () => { // export class Board extends Component
 
-  render() {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { squares: Array(9).fill(null) } } 
+
+  
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(squares);
+
+  const status = `Next Player ${xIsNext ? 'X' : 'O'}`;
+
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} 
+        onClick={() => handleClick(i)} />
+  } 
+
+  //render() { // class 컴포넌트에서는 return을 render 안에 해야한다.
     return (
       <div>
-        <div className='status'>Next Player: X, O</div>
+        <div className='status'>{status}</div>
         <div className='board-row'>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
         <div className='board-row'>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
         <div className='board-row'>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
         </div>
       </div>
-    )
-  }
+    )  
 }
+export default Board // 단축키 rafce
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,21 +1,32 @@
 import React from "react";
 import "./Square.css"
 
-export default class Square extends React.Component {
-
-    constructor(props) {
+// export default class Square extends React.Component {
+const Square = ({ onClick, value}) => {
+    
+    /*constructor(props) {
         super(props);
         this.state = {
             value: null,
         }
-    }
+    }*/
 
-    render() {
+    // render() { */ // 함수형 컴포넌트는 redner 사용 X
+    /* const Square = (props) => {
+       return (
+            <button className="square" 
+                //onClick={() => setState({value: 'X'})}>
+                onClick={() => props.onClick}>
+                {props.value}
+            </button>
+        ) */
+
         return (
-            <button className="square" onClick={() => this.setState({value: 'X'})}>
-                {/* {this.props.value} */}
-                {this.state.value}
+            <button className="square" 
+                onClick={onClick}>
+                {value}
             </button>
         )
-    }
 }
+
+export default Square
